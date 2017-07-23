1b051c344f482a97b684eacc257a348fc7d838b0
gaods <gaods4123>
2017-07-23 23:14:16 +0800

    添加需要的后台服务

---
+++ manager-domain/src/main/java/com/hesh/vo/user/UserVO.java
@@ -0,0 +1,28 @@
+package com.hesh.vo.user;
+
+/**
+ * Created by gaods on 2017/7/23.
+ */
+public class UserVO {
+
+
+    private String userName;
+
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
