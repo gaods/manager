f2bc353b7bd8761f3de9503ae3f4c09b85326afd
guojinquan3 <guojinquan1@jd.com>
2017-07-30 16:18:40 +0800

    jiekou

---
+++ manager-service/src/main/java/com/hesh/service/SanFangxcService.java
@@ -0,0 +1,18 @@
+package com.hesh.service;
+
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: guojinquan1
+ * Date: 17-7-30
+ * Time: 下午2:08
+ * To change this template use File | Settings | File Templates.
+ */
+public interface SanFangxcService {
+    public Map<String,Object>  getZcPhonePassWord(Map<String,Object>  map);
+
+    public Map<String, Object> getYzNumber(Map<String, Object> map);
+
+    public String getXingChenToken(Map<String,Object> map);
+}
