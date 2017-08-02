5146ad18e7c97bad5846b98946e6d8c560132bc1
guojinquan3 <guojinquan1@jd.com>
2017-08-02 17:20:48 +0800

    配置三方参数

--- manager-service/src/main/java/com/hesh/service/impl/SanFangxcServiceImpl.java
+++ manager-service/src/main/java/com/hesh/service/impl/SanFangxcServiceImpl.java
@@ -4,8 +4,9 @@ import com.hesh.service.SanFangxcService;
 //import com.hesheng.utils.GetRandomNumber;
 import com.hesheng.utils.GetRandomNumber;
 import com.hesheng.utils.HttpClientUtil;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
@@ -20,7 +21,7 @@ import java.util.Map;
  */
 @Service("sanFangxcService")
 public class SanFangxcServiceImpl implements SanFangxcService {
-
+    private static final Log logger = LogFactory.getLog(SanFangxcServiceImpl.class);
     /**
      * 获取号码密码
      * @param map
@@ -30,13 +31,29 @@ public class SanFangxcServiceImpl implements SanFangxcService {
     public Map<String, Object> getZcPhonePassWord(Map<String, Object> map) {
         if(null!=map && null!=map.get("opFlag")){
             Map<String, Object> ppMap = new HashMap<String, Object>();
+            String phoneNumber = "";
             String xmid = "";
             String token = "";
             String sl = "";
             String url = "http://www.xingchenma.com:9180/service.asmx/GetHMStr?token=132A49069398917D4C666B1BA77242C8&xmid=200&sl=1&lx=0&a1=&a2=&pk=";
             String paPhone  = HttpClientUtil.httpGetRequest(url);
+            if(null!=paPhone&&!("").equals(paPhone)){
+                if(paPhone.length()>3){
+                    String code = paPhone.substring(0,2);
+                    if(code.equals("hm")){
+                        String phoneNumbers = paPhone.replace("hm=","");
+                        String[] paPhones = phoneNumbers.split(",");
+                        phoneNumber=paPhones[0];
+                        logger.info(paPhones);
+                    }
+                    if(code.equals("id")){
+                        String xid = paPhone.replaceAll("id=","");
+                        logger.info(xid);
+                    }
+                }
+            }
             String paPassWord = GetRandomNumber.generateLowerString(8);
-            ppMap.put("phoneNumber",paPhone);
+            ppMap.put("phoneNumber",phoneNumber);
             ppMap.put("passWord",paPassWord);
             return ppMap;
 
@@ -102,18 +119,25 @@ public class SanFangxcServiceImpl implements SanFangxcService {
      */
     @Override
     public String getXingChenToken(Map<String, Object> map) {
-        if(null!=map && null!=map.get("xcUserName") && null!=map.get("xcPassWord")){
-            String user = (String)map.get("xcUserName")  ;
-            String passWord = (String)map.get("xcPassWord")  ;
-            String url ="http://www.xingchenma.com:9180/service.asmx/UserLoginStr?name="+user+"&psw="+passWord;
-            String result =   HttpClientUtil.httpGetRequest(url) ;
-
-            if(StringUtils.isEmpty(result)){
-
+        try{
+            if(null!=map && null!=map.get("xcUserName") && null!=map.get("xcPassWord")){
+                String user = (String)map.get("xcUserName")  ;
+                String passWord = (String)map.get("xcPassWord")  ;
+                String url ="http://www.xingchenma.com:9180/service.asmx/UserLoginStr?name="+user+"&psw="+passWord;
+                String token =   HttpClientUtil.httpGetRequest(url) ;
+                //三方接口返回是否为空校验
+                if(null!=token && !("").equals(token)){
+                    if(token.length()>20){
+                        //TODO 存储token信息在缓存中，方便下次直接使用每次任务结束清理缓存信息
+                        return token;
+                    }
+                }else {
+                    logger.info("调用接口超时异常"+token);
+                    return null;
+                }
             }
-
-
-            return result;
+        } catch (Exception ex){
+              logger.error("getXingChenToken"+ex);
         }
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
