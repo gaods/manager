f94cb74a2abc52160923352550016193a786a5ae
guojinquan3 <guojinquan1@jd.com>
2017-08-05 11:08:01 +0800

    增加存储三方信息

---
+++ manager-service/src/main/java/com/hesh/dao/SanFangMapper.java
@@ -0,0 +1,13 @@
+package com.hesh.dao;
+
+import com.hesh.vo.user.Customer;
+import com.hesh.vo.user.SanFang;
+
+import java.util.List;
+
+/**
+ * Created by gaods on 2017/7/30.
+ */
+public interface SanFangMapper {
+  public   SanFang getSanFangInfoById(int id);
+}
